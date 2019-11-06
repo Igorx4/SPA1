3bee635c7f6c8b07ea777c85b9a4bec8411c0b61
igor <kursov.ig1@gmail.com>
2019-11-06 20:57:27 +0200

    3 bloka

--- js/script.js
+++ js/script.js
@@ -13,29 +13,31 @@ $(document).ready(function(){
 	if ($(window).width() <= 1400) {
 		$('.title p').html('Забронируй SPA со скидкой 20%');
 	}  
+
+	$( "a.scrollLink" ).click(function( event ) {
+		event.preventDefault();
+		$("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 1000);
+	});
 });
 
 
-$( "a.scrollLink" ).click(function( event ) {
-	event.preventDefault();
-	$("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 1000);
-});
+
 
 
 $(window).scroll(function(){
 	let scrTop = $(window).scrollTop();
 	if ($(window).width() >= 1200) {
 		$('.title').css({"transform": "translate(0%, -"+ scrTop/15 + "%"});
-		if (scrTop > 1200) {
-			$('.services-item').css({"display" : "flex"});
-			$('.animate').addClass('scale');
-		};
-		if (scrTop > 700) {
-			$('.description-item').css({"display" : "block"});
-			$('.description-item:nth-child(1)').addClass('translateLeft');
-			$('.description-item:nth-child(2)').addClass('scale');
-			$('.description-item:nth-child(3)').addClass('translateRight')
-		};
+		// if (scrTop > 1200) {
+		// 	$('.services-item').css({"display" : "flex"});
+		// 	$('.animate').addClass('scale');
+		// };
+		// if (scrTop > 700) {
+		// 	$('.description-item').css({"display" : "block"});
+		// 	$('.description-item:nth-child(1)').addClass('translateLeft');
+		// 	$('.description-item:nth-child(2)').addClass('scale');
+		// 	$('.description-item:nth-child(3)').addClass('translateRight')
+		// };
 	}
 	const breakPoints = [
 		1000, 1600, 2800
@@ -75,36 +77,39 @@ let mediaMenu = () =>{
 
 let carousel = () =>{
 	if ($(window).width() <= 992) {
-		$('.services-carousel').addClass('services-carousel-media');
-		$('.screen-carousel').addClass('screen-carousel-media');
+		let slider = document.getElementById('screen-carousel');
+		let blockRight = $(window).width()/4;
+		if ($(window).width() <= 800) {
+			blockRight = $(window).width()/2.3;
+		}
+		if ($(window).width() <= 700) {
+			blockRight = $(window).width()/2;
+		}
+		if ($(window).width() <= 600) {
+			blockRight = $(window).width();
+		}
+		if ($(window).width() <= 500) {
+			blockRight = $(window).width();
+		}
+		if ($(window).width() <= 400) {
+			blockRight = $(window).width()*1.85;
+		}
+		slider.onmousedown = function(e) {
+			moveAt(e);
+			function moveAt(e) {
+				let x = e.pageX - slider.offsetWidth / 2;
+				if (x <= 0 && x > -blockRight) { 
+					slider.style.left = x + 'px';
+				}
+			}
+			document.onmousemove = function(e) {
+				moveAt(e);
+			}
+			slider.onmouseup = function() {
+				document.onmousemove = null;
+				slider.onmouseup = null;
+			}
+		}
 	}
 }
 
-
-let slider = document.querySelector('.screen-carousel');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown',(e)=> {
-	isDown = true;
-	slider.classList.add('active');
-	startX = e.pageX - slider.offsetLeft;
-	scrollLeft = slider.style.left;
-});
-slider.addEventListener('mouseleave',(e)=> {
-	isDown = false;
-	slider.classList.remove('active');
-});
-slider.addEventListener('mouseup',(e)=> {
-	isDown = false;
-	slider.classList.remove('active');
-});
-slider.addEventListener('mousemove',(e)=> {
-	if(!isDown) return;
-	e.preventDefault();
-	const x = e.pageX - slider.offsetLeft;
-	const walk = x-startX;
-	slider.style.left = walk + 'px';
-	console.log({x,startX});
-});
